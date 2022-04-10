f348b8d52ab3862a9564455d473d728aac9b3f09
John Ellis <john@seriouslyawesome.com>
2022-04-09 20:05:14 -0500

    Don't reset $HOME. That's bad.

--- .zshrc
+++ .zshrc
@@ -1,22 +1,18 @@
-# Set a new HOME directory, and then...
-HOME="/Users/$USER/Development"
-export HOME
-
-# ...immediately switch into that directory.
-cd ~
+DEVHOME=~/Development
+cd $DEVHOME
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+if [[ -r "${XDG_CACHE_HOME:-$DEVHOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$DEVHOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
 # If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+# export PATH=$DEVHOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="$HOME/.oh-my-zsh"
+export ZSH="$DEVHOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
@@ -122,4 +118,7 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+[[ ! -f "$DEVHOME/.p10k.zsh" ]] || source "$DEVHOME/.p10k.zsh"
+
+# Initialize homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
--- install.sh
+++ install.sh
@@ -1,26 +1,19 @@
 
-# Set a new HOME directory, and then...
-echo "Setting new home directory..."
-HOME="/Users/$USER/Development"
-export HOME
-
-# ...immediately switch into that directory.
-cd ~
-echo "...done! Home directory set to $HOME"
+DEVHOME=~/Development
 
 # Install oh-my-zsh
 echo "Installing oh-my-zsh for a more delightful shell experience..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+sh -c "ZSH='$DEVHOME/.oh-my-zsh' $(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 echo "...done! oh-my-zsh was installed!"
 
 # Install powerlevel10k
 echo "Installing powerlevel10k theme for oh-my-zsh for snazzy shell prompts..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$DEVHOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "...done! Your prompts will be snazzified shortly!"
 
 # Install zsh-autosuggestions
 echo "Installing zsh-autosuggestions for helpful hints when typing..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$DEVHOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "...done! zsh-autosuggestions installed!"
 
 # Create a symlink to point the user's default .zshrc to the dotfiles repo
@@ -42,7 +35,7 @@ git config --global core.excludesfile ~/dotfiles/.gitignore
 echo "...done!"
 
 echo "Just about finished! Either close this window and open a new one to get started, or run this command:"
-echo "\n    source ~/dotfiles/.zshrc\n"
+echo "\n    source ~/Development/dotfiles/.zshrc\n"
 
 # Initialize all our new shit!
 exec zsh
